95c2af4e95528b7c4e40b1cdf75e517f37414416
rohit-newdevloper <70029207+rohit-newdevloper@users.noreply.github.com>
2020-10-01 07:53:24 +0530

    just themes color copied

--- frontend/src/styles/themes/dark.ts
+++ frontend/src/styles/themes/dark.ts
@@ -17,4 +17,17 @@ export default {
     inputPlaceholder: '#666360',
     insideButton: '#FFFFFF'
   },
+  {
+    primary: '#312e38',
+    secundary: 'linear-gradient(270deg, #FFE53B 0%, #FF2525 100%)',
+
+    border: '#232129',
+    background: '#312E38',
+    li: '#232129',
+    toggle: '#FFFFFF',
+    text: '#FFFFFF',
+    inputColor: '#232129',
+    inputPlaceholder: '#666360',
+    insideButton: '#FFFFFF'
+  },
 }
